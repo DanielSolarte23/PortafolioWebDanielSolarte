feed827a2e3abb2fbcfb171941f3c0d8d1209b65
Daniel Solarte <royersolarte22@gmail.com>
2024-03-15 14:51:07 -0500

    Portafolio web Daniel Solarte v1.0

---
+++ js/Calculadora.js
@@ -0,0 +1,14 @@
+class Calculadora{
+    sumar(num, num2){
+        return num + num2
+    } 
+    restar(num, num2){
+        return num - num2
+    } 
+    dividir(num, num2){
+        return num / num2
+    } 
+    multiplicar(num, num2){
+        return num * num2
+    } 
+}
